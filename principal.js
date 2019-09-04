f8cb874f686d6ddaf9d91749a9b94710a1243410
Jardel Rocha <jardelrochaps@gmail.com>
2019-09-04 19:37:12 -0300

    Inserindo informações do menu O Curso

---
+++ principal.js
@@ -0,0 +1,12 @@
+var banners = 
+	["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bannerAtual = 0;
+
+
+funcion trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('h2#mensagem').textContent =
+		banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
